e9ab079b7c9197667be33f1149571a369eeae081
chenyuyuan <1695215206@qq.com>
2019-06-24 10:46:43 +0800

    edit tempe

--- dev/tempe.py
+++ dev/tempe.py
@@ -5,8 +5,8 @@ TempPin = 31
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-	GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
-	GPIO.add_event_detect(BtnPin, GPIO.BOTH, callback=detect, bouncetime=200)
+	GPIO.setup(TempPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
+	GPIO.add_event_detect(TempPin, GPIO.BOTH, callback=detect, bouncetime=200)
 
         
 def detect(chn):
