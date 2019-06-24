05f1be55e5251a212d9ad2964ab7d099d97a919f
chenyuyuan <1695215206@qq.com>
2019-06-24 10:38:57 +0800

    add dev

---
+++ dev/temperature.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+
+TempPin = 31
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+	GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
+	GPIO.add_event_detect(BtnPin, GPIO.BOTH, callback=detect, bouncetime=200)
+
+        
+def detect(chn):
+	print('Tempearture is 'GPIO.input(BtnPin))
+
+def loop():
+	while True:
+		pass
+
+def destroy():
+	GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
+
